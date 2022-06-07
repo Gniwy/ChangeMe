079939f7d3a235bc6577d779b43f965258c6f540
Cristian <cristiancastroalarcon@gmail.com>
2022-06-07 17:23:49 +0200

    Pages, components

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CabeceraComponent } from './cabecera/cabecera.component';
-import { SliderComponent } from './slider/slider.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    SliderComponent
+    SliderComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
